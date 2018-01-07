000dc70dbf695a8b7e1fa61e61ecc6d7d8e767b0
AlexeenkoRodion@yandex.by <AlexeenkoRodion@yandex.by>
2018-01-07 17:56:50 +0300

    first commit

---
+++ ImgParser/Main.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ImgParser
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(55, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(322, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(179, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите сайт";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(160, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 58);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "парсить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(55, 140);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(40, 20);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "глубина";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 365);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Main";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ImgParser/Main.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace ImgParser
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists("Last parsing"))
+                Directory.Delete("Last parsing", true);
+            Directory.CreateDirectory("Last parsing");
+
+            string address = textBox1.Text;
+            int depth = (int)numericUpDown1.Value;
+
+            if (NativeMethods.AllocConsole())
+            {
+                IntPtr stdHandle = NativeMethods.GetStdHandle(NativeMethods.STD_OUTPUT_HANDLE);
+            }
+
+            DonwloadPictures("Last parsing/", address);
+        }
+
+        public void DonwloadPictures(string to, string address)
+        {
+            WebClient wcl = new WebClient();
+            
+            foreach (var el in ParseLinksOnPictures(OpenPage(address)))
+            {
+                string s = RemoveBannedSymbols(el);
+
+                Console.WriteLine(s);
+
+                try { wcl.DownloadFile(el, "Last parsing/" + s + ".jpg"); }
+                catch { }
+                try { wcl.DownloadFile(address + '/' + el, "Last parsing/" + s + "2.jpg"); }
+                catch { }
+            }
+
+            wcl.Dispose();
+        }
+
+        public string OpenPage(string address)
+        {
+            WebClient wcl = new WebClient();
+
+            string ans = Encoding.UTF8.GetString(wcl.DownloadData(address));
+            Console.WriteLine(ans);
+
+            wcl.Dispose();
+
+            return ans;
+        }
+
+        public string RemoveBannedSymbols(string s)
+        {
+            char[] chs = "\\/:*?\"<>|".ToCharArray();
+
+            foreach (char ch in chs)
+            {
+                s = s.Replace(ch.ToString(), "");
+            }
+
+            return s;
+        }
+
+        public List<string> ParseLinksOnPictures(string ans)
+        {
+            List<string> list = new List<string>();
+
+            int pos = 0;
+            while ((pos = ans.IndexOf("<img", pos)) != -1)
+                while (ans[pos] != '>')
+                {
+                    if (ans[pos] == 's' && ans[pos + 1] == 'r' && ans[pos + 2] == 'c' && ans[pos + 3] == '=' && ans[pos + 4] == '"')
+                    {
+                        StringBuilder sb = new StringBuilder(100);
+
+                        pos += 5;
+                        while (ans[pos] != '"')
+                        {
+                            sb.Append(ans[pos], 1);
+                            pos++;
+                        }
+
+                        list.Add(sb.ToString());
+                    }
+
+                    pos++;
+                }
+
+            return list;
+        }
+
+        public class NativeMethods
+        {
+            public static Int32 STD_OUTPUT_HANDLE;
+
+            [System.Runtime.InteropServices.DllImportAttribute("kernel32.dll", EntryPoint = "GetStdHandle")]
+            public static extern IntPtr GetStdHandle(Int32 nStdHandle);
+
+            [System.Runtime.InteropServices.DllImportAttribute("kernel32.dll", EntryPoint = "AllocConsole")]
+            [return: System.Runtime.InteropServices.MarshalAsAttribute(System.Runtime.InteropServices.UnmanagedType.Bool)]
+            public static extern bool AllocConsole();
+        }
+    }
+}
