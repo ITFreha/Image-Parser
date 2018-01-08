7b5e3845e8b2478723b19ce459ff403beef1ce4a
AlexeenkoRodion@yandex.by <AlexeenkoRodion@yandex.by>
2018-01-09 01:28:25 +0300

    improved

--- ImgParser/Main.Designer.cs
+++ ImgParser/Main.Designer.cs
@@ -30,66 +30,241 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.parsingbutton = new System.Windows.Forms.Button();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.task1label = new System.Windows.Forms.Label();
+            this.task1text = new System.Windows.Forms.TextBox();
+            this.stoptask1button = new System.Windows.Forms.Button();
+            this.stoptask2button = new System.Windows.Forms.Button();
+            this.task2text = new System.Windows.Forms.TextBox();
+            this.task2label = new System.Windows.Forms.Label();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.stoptask3button = new System.Windows.Forms.Button();
+            this.task3text = new System.Windows.Forms.TextBox();
+            this.task3label = new System.Windows.Forms.Label();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.opendirbutton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(55, 38);
+            this.textBox1.Location = new System.Drawing.Point(64, 50);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(322, 20);
+            this.textBox1.Size = new System.Drawing.Size(417, 20);
             this.textBox1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(179, 13);
+            this.label1.Font = new System.Drawing.Font("Perpetua", 16.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(196, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.Size = new System.Drawing.Size(125, 27);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Введите сайт";
+            this.label1.Text = "Input website";
             // 
-            // button1
+            // parsingbutton
             // 
-            this.button1.Location = new System.Drawing.Point(160, 74);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(119, 58);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "парсить";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.parsingbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.parsingbutton.Location = new System.Drawing.Point(371, 92);
+            this.parsingbutton.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.parsingbutton.Name = "parsingbutton";
+            this.parsingbutton.Size = new System.Drawing.Size(110, 60);
+            this.parsingbutton.TabIndex = 2;
+            this.parsingbutton.Text = "do parse";
+            this.parsingbutton.UseVisualStyleBackColor = true;
+            this.parsingbutton.Click += new System.EventHandler(this.parsingbutton_Click);
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(55, 140);
+            this.numericUpDown1.Location = new System.Drawing.Point(69, 121);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(40, 20);
+            this.numericUpDown1.Size = new System.Drawing.Size(38, 20);
             this.numericUpDown1.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(52, 110);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(63, 96);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.Size = new System.Drawing.Size(50, 20);
             this.label2.TabIndex = 4;
-            this.label2.Text = "глубина";
+            this.label2.Text = "depth";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.Location = new System.Drawing.Point(185, 96);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(120, 24);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "in one folder";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(64, 168);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(196, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // task1label
+            // 
+            this.task1label.AutoSize = true;
+            this.task1label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.task1label.Location = new System.Drawing.Point(9, 168);
+            this.task1label.Name = "task1label";
+            this.task1label.Size = new System.Drawing.Size(49, 20);
+            this.task1label.TabIndex = 7;
+            this.task1label.Text = "task1";
+            // 
+            // task1text
+            // 
+            this.task1text.Location = new System.Drawing.Point(266, 170);
+            this.task1text.Name = "task1text";
+            this.task1text.ReadOnly = true;
+            this.task1text.Size = new System.Drawing.Size(112, 20);
+            this.task1text.TabIndex = 8;
+            this.task1text.Text = "empty";
+            // 
+            // stoptask1button
+            // 
+            this.stoptask1button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.stoptask1button.Location = new System.Drawing.Point(406, 168);
+            this.stoptask1button.Name = "stoptask1button";
+            this.stoptask1button.Size = new System.Drawing.Size(75, 23);
+            this.stoptask1button.TabIndex = 9;
+            this.stoptask1button.Text = "stop";
+            this.stoptask1button.UseVisualStyleBackColor = true;
+            this.stoptask1button.Click += new System.EventHandler(this.stoptask1button_Click);
+            // 
+            // stoptask2button
+            // 
+            this.stoptask2button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.stoptask2button.Location = new System.Drawing.Point(406, 197);
+            this.stoptask2button.Name = "stoptask2button";
+            this.stoptask2button.Size = new System.Drawing.Size(75, 23);
+            this.stoptask2button.TabIndex = 13;
+            this.stoptask2button.Text = "stop";
+            this.stoptask2button.UseVisualStyleBackColor = true;
+            this.stoptask2button.Click += new System.EventHandler(this.stoptask2button_Click);
+            // 
+            // task2text
+            // 
+            this.task2text.Location = new System.Drawing.Point(266, 199);
+            this.task2text.Name = "task2text";
+            this.task2text.ReadOnly = true;
+            this.task2text.Size = new System.Drawing.Size(112, 20);
+            this.task2text.TabIndex = 12;
+            this.task2text.Text = "empty";
+            // 
+            // task2label
+            // 
+            this.task2label.AutoSize = true;
+            this.task2label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.task2label.Location = new System.Drawing.Point(9, 197);
+            this.task2label.Name = "task2label";
+            this.task2label.Size = new System.Drawing.Size(49, 20);
+            this.task2label.TabIndex = 11;
+            this.task2label.Text = "task2";
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(64, 197);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(196, 23);
+            this.progressBar2.TabIndex = 10;
+            // 
+            // stoptask3button
+            // 
+            this.stoptask3button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.stoptask3button.Location = new System.Drawing.Point(406, 226);
+            this.stoptask3button.Name = "stoptask3button";
+            this.stoptask3button.Size = new System.Drawing.Size(75, 23);
+            this.stoptask3button.TabIndex = 17;
+            this.stoptask3button.Text = "stop";
+            this.stoptask3button.UseVisualStyleBackColor = true;
+            this.stoptask3button.Click += new System.EventHandler(this.stoptask3button_Click);
+            // 
+            // task3text
+            // 
+            this.task3text.Location = new System.Drawing.Point(266, 228);
+            this.task3text.Name = "task3text";
+            this.task3text.ReadOnly = true;
+            this.task3text.Size = new System.Drawing.Size(112, 20);
+            this.task3text.TabIndex = 16;
+            this.task3text.Text = "empty";
+            // 
+            // task3label
+            // 
+            this.task3label.AutoSize = true;
+            this.task3label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.task3label.Location = new System.Drawing.Point(9, 226);
+            this.task3label.Name = "task3label";
+            this.task3label.Size = new System.Drawing.Size(49, 20);
+            this.task3label.TabIndex = 15;
+            this.task3label.Text = "task3";
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(64, 226);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(196, 23);
+            this.progressBar3.TabIndex = 14;
+            // 
+            // opendirbutton
+            // 
+            this.opendirbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.opendirbutton.Location = new System.Drawing.Point(12, 9);
+            this.opendirbutton.Name = "opendirbutton";
+            this.opendirbutton.Size = new System.Drawing.Size(75, 30);
+            this.opendirbutton.TabIndex = 18;
+            this.opendirbutton.Text = "open dir";
+            this.opendirbutton.UseVisualStyleBackColor = true;
+            this.opendirbutton.Click += new System.EventHandler(this.opendirbutton_Click);
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(459, 365);
+            this.ClientSize = new System.Drawing.Size(504, 271);
+            this.Controls.Add(this.opendirbutton);
+            this.Controls.Add(this.stoptask3button);
+            this.Controls.Add(this.task3text);
+            this.Controls.Add(this.task3label);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.stoptask2button);
+            this.Controls.Add(this.task2text);
+            this.Controls.Add(this.task2label);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.stoptask1button);
+            this.Controls.Add(this.task1text);
+            this.Controls.Add(this.task1label);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.parsingbutton);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Name = "Main";
-            this.Text = "Form1";
+            this.Opacity = 0.97D;
+            this.Text = "Image parser";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -100,9 +275,23 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button parsingbutton;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label task1label;
+        private System.Windows.Forms.TextBox task1text;
+        private System.Windows.Forms.Button stoptask1button;
+        private System.Windows.Forms.Button stoptask2button;
+        private System.Windows.Forms.TextBox task2text;
+        private System.Windows.Forms.Label task2label;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Button stoptask3button;
+        private System.Windows.Forms.TextBox task3text;
+        private System.Windows.Forms.Label task3label;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.Button opendirbutton;
     }
 }
 
--- ImgParser/Main.cs
+++ ImgParser/Main.cs
@@ -2,53 +2,222 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ImgParser
 {
     public partial class Main : Form
     {
+        private CancellationTokenSource cts1;
+        private CancellationTokenSource cts2;
+        private CancellationTokenSource cts3;
+
         public Main()
         {
             InitializeComponent();
-        }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if (Directory.Exists("Last parsing"))
-                Directory.Delete("Last parsing", true);
-            Directory.CreateDirectory("Last parsing");
-
-            string address = textBox1.Text;
-            int depth = (int)numericUpDown1.Value;
-
+            /*
             if (NativeMethods.AllocConsole())
             {
                 IntPtr stdHandle = NativeMethods.GetStdHandle(NativeMethods.STD_OUTPUT_HANDLE);
             }
-
-            DonwloadPictures("Last parsing/", address);
+            //*/
+            stoptask1button.Enabled = false;
+            stoptask2button.Enabled = false;
+            stoptask3button.Enabled = false;
         }
 
-        public void DonwloadPictures(string to, string address)
+        private void parsingbutton_Click(object sender, EventArgs e)
         {
+            string address = textBox1.Text;
+            int depth = (int)numericUpDown1.Value;
+
+            if (address.Length > 0 && address[address.Length - 1] != '/')
+                address = address + "/";
+
+            if (!stoptask1button.Enabled)
+            {
+                cts1 = new CancellationTokenSource();
+
+                Task.Factory.StartNew(() => StartQueue(address, depth, cts1, task1text, progressBar1, stoptask1button));
+                stoptask1button.Enabled = true;
+            }
+            else if (!stoptask2button.Enabled)
+            {
+                cts2 = new CancellationTokenSource();
+
+                Task.Factory.StartNew(() => StartQueue(address, depth, cts2, task2text, progressBar2, stoptask2button));
+                stoptask2button.Enabled = true;
+            }
+            else if (!stoptask3button.Enabled)
+            {
+                cts3 = new CancellationTokenSource();
+
+                Task.Factory.StartNew(() => StartQueue(address, depth, cts3, task3text, progressBar3, stoptask3button));
+                stoptask3button.Enabled = true;
+            }
+
+            if (stoptask1button.Enabled && stoptask2button.Enabled && stoptask3button.Enabled)
+            {
+                parsingbutton.Enabled = false;
+            }
+        }
+
+        private void stoptask1button_Click(object sender, EventArgs e)
+        {
+            Cancel(cts1, task1text, stoptask1button);
+        }
+
+        private void stoptask2button_Click(object sender, EventArgs e)
+        {
+            Cancel(cts2, task2text, stoptask2button);
+        }
+
+        private void stoptask3button_Click(object sender, EventArgs e)
+        {
+            Cancel(cts3, task3text, stoptask3button);
+        }
+
+        private void opendirbutton_Click(object sender, EventArgs e)
+        {
+            string dir = Directory.GetCurrentDirectory();
+            Process.Start(dir);
+        }
+
+        private void Cancel(CancellationTokenSource cts, TextBox tasktext, Button stoptaskbutton)
+        {
+            cts.Cancel();
+            tasktext.Text = "aborted";
+            stoptaskbutton.Enabled = false;
+
+            parsingbutton.Enabled = true;
+        }
+
+        public struct Element
+        {
+            public int d;
+            public string link;
+
+            public Element(string link_on_page, int depth)
+            {
+                link = link_on_page;
+                d = depth;
+            }
+        }
+
+        public void StartQueue(string address, int depth, 
+            CancellationTokenSource cts, TextBox tasktext, ProgressBar progress, Button stoptaskbutton)
+        {
+            if (OpenPage(address) == null)
+            {
+                Cancel(cts, tasktext, stoptaskbutton);
+                progress.Value = progress.Minimum;
+                tasktext.Text = "Open error";
+                return;
+            }
+
+            tasktext.Text = address;
+            Task.Factory.StartNew(() => {
+                progress.Minimum = 0;
+                progress.Maximum = 10000;
+
+                while (true)
+                {
+                    progress.Value = 0;
+                    for (int i = 0; i < 10300; i++)
+                    {
+                        progress.Increment(1);
+                        Thread.Sleep(1);
+
+                        cts.Token.ThrowIfCancellationRequested();
+                    }
+                }
+            }, cts.Token);
+
+            HashSet<string> set = new HashSet<string>();
+            Queue<Element> queue = new Queue<Element>();
+            queue.Enqueue(new Element(address, 0));
+
+            Element t;
+            while (queue.Count != 0)
+            {
+                t = queue.Dequeue();
+
+                if (t.d > depth)
+                    break;
+
+                Console.WriteLine("_____________________" + t.d + "_____________________");
+
+                string ans = OpenPage(t.link);
+                if (ans == null)
+                    continue;
+
+                List<string> pictsLinks = ParseLinksOnPictures(ans);
+                ParseLinksOnPages(address, ans, queue, t.d, set);
+
+                string path = RemoveBannedSymbols(t.link, true) + "/";
+                string supposedMainAdress = PickOutMainAddress(t.link);
+                DownloadPictures(supposedMainAdress, address, path, pictsLinks, cts);
+
+                if (cts.IsCancellationRequested)
+                    return;
+            }
+
+            cts.Cancel();
+            stoptaskbutton.Enabled = false;
+            tasktext.Text = "completed";
+            progress.Value = progressBar1.Maximum;
+
+            parsingbutton.Enabled = true;
+        }
+
+        string PickOutMainAddress(string address)
+        {
+            int pos = 8;
+            while (address[pos] != '/')
+                pos++;
+
+            return address.Substring(0, pos + 1);
+        }
+
+        public void DownloadPictures(string supposedMainAdress, string address, string path,
+            List<string> links, CancellationTokenSource cts)
+        {
+            if (checkBox1.Checked)
+                path = RemoveBannedSymbols(address, true) + "/";
+
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+
             WebClient wcl = new WebClient();
             
-            foreach (var el in ParseLinksOnPictures(OpenPage(address)))
+            foreach (var link in links)
             {
-                string s = RemoveBannedSymbols(el);
+                if (cts.IsCancellationRequested)
+                    return;
 
-                Console.WriteLine(s);
+                string name = RemoveBannedSymbols(link, false);
 
-                try { wcl.DownloadFile(el, "Last parsing/" + s + ".jpg"); }
+                Console.WriteLine(link);
+
+                try { wcl.DownloadFile(link, path + name + ".jpg"); }
                 catch { }
-                try { wcl.DownloadFile(address + '/' + el, "Last parsing/" + s + "2.jpg"); }
+                try { wcl.DownloadFile(address + '/' + link, path + name + "2.jpg"); }
+                catch { }
+                try { wcl.DownloadFile("http:" + link, path + name + "3.jpg"); }
+                catch { }
+                try { wcl.DownloadFile("http://" + link, path + name + "4.jpg"); }
+                catch { }
+                try { wcl.DownloadFile(supposedMainAdress + link, path + name + "5.jpg"); }
                 catch { }
             }
 
@@ -57,17 +226,23 @@ namespace ImgParser
 
         public string OpenPage(string address)
         {
-            WebClient wcl = new WebClient();
+            string ans;
 
-            string ans = Encoding.UTF8.GetString(wcl.DownloadData(address));
-            Console.WriteLine(ans);
+            try
+            {
+                WebClient wcl = new WebClient();
 
-            wcl.Dispose();
+                ans = Encoding.UTF8.GetString(wcl.DownloadData(address));
+                //Console.WriteLine(ans);
+
+                wcl.Dispose();
+            }
+            catch { ans = null; }
 
             return ans;
         }
 
-        public string RemoveBannedSymbols(string s)
+        public string RemoveBannedSymbols(string s, bool forDir)
         {
             char[] chs = "\\/:*?\"<>|".ToCharArray();
 
@@ -75,6 +250,9 @@ namespace ImgParser
             {
                 s = s.Replace(ch.ToString(), "");
             }
+
+            if (!forDir)
+                s = s.Remove(s.Length / 4, s.Length / 2);
 
             return s;
         }
@@ -105,6 +283,45 @@ namespace ImgParser
                 }
 
             return list;
+        }
+
+        public void ParseLinksOnPages(string address, string ans, Queue<Element> queue, int depth, HashSet<string> set)
+        {
+            int pos = 0;
+            while ((pos = ans.IndexOf("<a", pos)) != -1)
+                while (ans[pos] != '>')
+                {
+                    if (ans[pos] == 'h' && ans[pos + 1] == 'r' && ans[pos + 2] == 'e' && ans[pos + 3] == 'f' && ans[pos + 4] == '=' && ans[pos + 5] == '"')
+                    {
+                        StringBuilder sb = new StringBuilder(100);
+
+                        pos += 6;
+                        while (ans[pos] != '"')
+                        {
+                            sb.Append(ans[pos], 1);
+                            pos++;
+                        }
+
+                        string link = sb.ToString();
+
+                        if (!set.Contains(link))
+                        {
+                            set.Add(link);
+
+                            queue.Enqueue(new Element(
+                                link,
+                                depth + 1
+                            ));
+
+                            queue.Enqueue(new Element(
+                                address + "/" + link,
+                                depth + 1
+                            ));
+                        }
+                    }
+
+                    pos++;
+                }
         }
 
         public class NativeMethods
